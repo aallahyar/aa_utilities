35c99671fc528db475580968a322517dcc8d6cd2
aallahyar <amin8w@gmail.com>
2025-04-05 19:54:55 +0200

    minor update

--- src/my_utilities/convenience/__init__.py
+++ src/my_utilities/convenience/__init__.py
@@ -1,5 +1,5 @@
 from ._convenience import *
-from ._containers import *
 from ._formaters import PrettyPrinter
+from ._containers import *
 from ._linkage_tree_parser import LinkageTreeParser
 from ._restricted_logger import Logger
