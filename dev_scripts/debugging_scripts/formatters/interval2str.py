dc03c864ad656c6c0daa9f3d2581b37d9fd7407e
aallahyar <amin8w@gmail.com>
2026-01-22 09:49:43 +0100

    added the first version of stability selection; it requires reviewing

---
+++ dev_scripts/debugging_scripts/formatters/interval2str.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+from aa_utilities.helpers import (
+    interval2str
+)
+
+print(interval2str(pd.Interval(0.123456, 0.654321)))
+print(interval2str(pd.Interval(1, 2, closed='both')))
+print(interval2str(pd.Interval(1, 2, closed='left')))
+print(interval2str(pd.Interval(1, 2, closed='right')))
+print(interval2str(pd.Interval(1, 2, closed='neither')))
+
+print(interval2str(pd.Interval(0.123456, 0.654321), fmt='{:.3f} <-> {:.3f}'))
+
+print(interval2str(np.nan))
+
+intv = pd.IntervalIndex.from_tuples([(0, 1), (1, 2), (2, 3)], closed='left')
+print(interval2str(intv, fmt='{:.0f}, {:.3f}'))
+
