3b0a2dd78c739cf6b7e679e20858683c219e5f27
aallahyar <amin8w@gmail.com>
2026-06-17 13:04:08 +0200

    update on watermark

--- src/aa_utilities/wrappers/_watermark.py
+++ src/aa_utilities/wrappers/_watermark.py
@@ -33,12 +33,13 @@ def watermark(
     Example:
         ```
         from aa_utilities.wrappers import watermark as aa_watermark
-        aa_watermark(
+        output_str = aa_watermark(
             author="Amin Allahyar",
             email='Amin.Allahyar@astrazeneca.com',
             timezone='Europe/Stockholm',
             namespace=globals(),
         )
+        print(output_str)
         ```
     """
     sections = []
